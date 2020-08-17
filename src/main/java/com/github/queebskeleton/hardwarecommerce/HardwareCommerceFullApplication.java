0f50b96fbca177f3bb5ba979aa6c523d6cb9a29e
QueebSkeleton <queebskeleton@gmail.com>
2020-08-17 14:17:38 +0800

    Initial commit

---
+++ src/main/java/com/github/queebskeleton/hardwarecommerce/HardwareCommerceFullApplication.java
@@ -0,0 +1,13 @@
+package com.github.queebskeleton.hardwarecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareCommerceFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareCommerceFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/queebskeleton/hardwarecommerce/HardwareCommerceFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.queebskeleton.hardwarecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareCommerceFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
