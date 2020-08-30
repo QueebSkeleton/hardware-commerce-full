20197e4aaadd51b3860a9bf7b73d8d743961fa4e
QueebSkeleton <queebskeleton@gmail.com>
2020-08-30 16:04:47 +0800

    Add product management panel administrator page

--- src/main/java/com/github/queebskeleton/hardwarecommerce/config/WebMvcConfiguration.java
+++ src/main/java/com/github/queebskeleton/hardwarecommerce/config/WebMvcConfiguration.java
@@ -22,6 +22,9 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 		// Administrator - Manage Vendors Panel
 		registry.addViewController("/admin/vendors").setViewName("admin/pages/vendor-mgmt/panel");
 		
+		// Administrator - Manage Products Panel
+		registry.addViewController("/admin/products").setViewName("admin/pages/product-mgmt/panel");
+		
 	}
 
 }
