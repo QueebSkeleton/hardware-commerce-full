a5f359de900f3a8614422c75b6efb8e8db400704
QueebSkeleton <queebskeleton@gmail.com>
2020-09-14 19:41:21 +0800

    Extend EntityGraphJpaRepository on ProductJpaRepository interface bean
    - Refactor ProductServiceImpl; generate EntityGraph then pass as
    parameter

--- src/main/java/com/github/queebskeleton/hardwarecommerce/repository/ProductJpaRepository.java
+++ src/main/java/com/github/queebskeleton/hardwarecommerce/repository/ProductJpaRepository.java
@@ -1,24 +1,12 @@
 package com.github.queebskeleton.hardwarecommerce.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import com.cosium.spring.data.jpa.entity.graph.repository.EntityGraphJpaRepository;
+import com.cosium.spring.data.jpa.entity.graph.repository.EntityGraphJpaSpecificationExecutor;
 import com.github.queebskeleton.hardwarecommerce.entity.Product;
 
 @Repository
-public interface ProductJpaRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
-	
-	@Override
-	@EntityGraph(attributePaths = { "category", "vendor" })
-	Page<Product> findAll(Pageable pageable);
-	
-	@Override
-	@EntityGraph(attributePaths = { "category", "vendor" })
-	Page<Product> findAll(Specification<Product> specs, Pageable pageable);
+public interface ProductJpaRepository extends EntityGraphJpaRepository<Product, Long>, EntityGraphJpaSpecificationExecutor<Product> {
 	
 }
