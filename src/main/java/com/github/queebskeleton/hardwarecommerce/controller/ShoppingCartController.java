485bc2e85d7008c046dfab911d1f9d173c6e81e5
QueebSkeleton <queebskeleton@gmail.com>
2020-09-17 14:09:39 +0800

    Support updating quantity if shopping cart item already exists

--- src/main/java/com/github/queebskeleton/hardwarecommerce/controller/ShoppingCartController.java
+++ src/main/java/com/github/queebskeleton/hardwarecommerce/controller/ShoppingCartController.java
@@ -1,16 +1,12 @@
 package com.github.queebskeleton.hardwarecommerce.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.cosium.spring.data.jpa.entity.graph.domain.EntityGraphUtils;
-import com.github.queebskeleton.hardwarecommerce.entity.Product;
 import com.github.queebskeleton.hardwarecommerce.model.ShoppingCart;
-import com.github.queebskeleton.hardwarecommerce.repository.ProductJpaRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,29 +16,11 @@ import lombok.RequiredArgsConstructor;
 @Scope(WebApplicationContext.SCOPE_REQUEST)
 public class ShoppingCartController {
 	
-	@Value("${general.settings.sales-tax-rate}")
-	private double salesTaxRate;
-
-	private final ProductJpaRepository productJpaRepository;
 	private final ShoppingCart shoppingCart;
 	
 	@GetMapping("/add")
 	public String addToCart(Long productId, int quantity) {
-		// TODO: Move to a service layer
-		Product product = productJpaRepository.findById(productId, EntityGraphUtils.fromAttributePaths("images"))
-				.orElseThrow(() -> new IllegalArgumentException("Invalid Product ID."));
-		
-		shoppingCart.addItem(
-				new ShoppingCart.Item(
-						product.getId(),
-						product.getName(),
-						product.getImages() != null && !product.getImages().isEmpty() ?
-								product.getImages().get(0).getFileName().toString() :
-									null, 
-						quantity,
-						product.isTaxable() ?
-							product.getUnitPrice() + (product.getUnitPrice() * salesTaxRate) :
-								product.getUnitPrice()));
+		shoppingCart.addItem(productId, quantity);
 		
 		return "redirect:/store";
 	}
--- src/main/java/com/github/queebskeleton/hardwarecommerce/model/ShoppingCart.java
+++ src/main/java/com/github/queebskeleton/hardwarecommerce/model/ShoppingCart.java
@@ -3,19 +3,29 @@ package com.github.queebskeleton.hardwarecommerce.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.cosium.spring.data.jpa.entity.graph.domain.EntityGraphUtils;
+import com.github.queebskeleton.hardwarecommerce.entity.Product;
+import com.github.queebskeleton.hardwarecommerce.repository.ProductJpaRepository;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+// TODO: Probably abstract away this class, since this implementation is Spring-reliant
 @Component
 @Scope(value = WebApplicationContext.SCOPE_SESSION)
 public class ShoppingCart {
+	
+	@Value("${general.settings.sales-tax-rate}")
+	private double salesTaxRate;
+	
+	private final ProductJpaRepository productJpaRepository;
 	
 	@Data
 	@NoArgsConstructor
@@ -34,18 +44,50 @@ public class ShoppingCart {
 		
 	}
 	
+	@Getter
 	private List<Item> items;
 	
-	public void addItem(Item item) {
-		if(items == null)
-			items = new ArrayList<>();
+	public ShoppingCart(ProductJpaRepository productJpaRepository) {
+		this.productJpaRepository = productJpaRepository;
+		items = new ArrayList<>();
+	}
+	
+	public void addItem(Long productId, int quantity) {
+		if(itemExists(productId)) {
+			getItemByProductId(productId).setQuantity(quantity);
+			return;
+		}
 		
-		items.add(item);
+		Product product =
+				productJpaRepository.findById(productId, EntityGraphUtils.fromAttributePaths("images"))
+					.orElseThrow(() -> new IllegalArgumentException("Invalid Product ID."));
+		
+		items.add(new Item(
+				product.getId(),
+				product.getName(),
+				!product.getImages().isEmpty() ?
+					product.getImages().get(0).getFileName().toString() :
+						null,
+				quantity,
+				product.isTaxable() ?
+					product.getUnitPrice() + (product.getUnitPrice() * salesTaxRate) :
+						product.getUnitPrice()));
 	}
 	
 	public void removeItem(Long productId) {
-		if(items != null)
-			items.removeIf(item -> item.getProductId() == productId);
+		items.removeIf(item -> item.getProductId() == productId);
+	}
+	
+	public boolean itemExists(Long productId) {
+		return !items.parallelStream()
+				.noneMatch(item -> item.getProductId() == productId);
+	}
+	
+	public Item getItemByProductId(Long productId) {
+		return items.parallelStream()
+					.filter(item -> item.getProductId() == productId)
+					.findFirst()
+					.orElse(null);
 	}
 	
 	public int getTotalQuantity() {
