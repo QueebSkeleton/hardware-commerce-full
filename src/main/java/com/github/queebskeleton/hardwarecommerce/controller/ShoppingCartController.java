faf1f3552d8670d2e4ba07d2171f04cb84ba2c5d
QueebSkeleton <queebskeleton@gmail.com>
2020-09-20 16:43:32 +0800

    Move Front Store mappings to FronStoreController, proxy ShoppingCart

--- src/main/java/com/github/queebskeleton/hardwarecommerce/controller/ShoppingCartController.java
+++ src/main/java/com/github/queebskeleton/hardwarecommerce/controller/ShoppingCartController.java
@@ -1,10 +1,8 @@
 package com.github.queebskeleton.hardwarecommerce.controller;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.github.queebskeleton.hardwarecommerce.model.ShoppingCart;
 
@@ -13,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/cart")
-@Scope(WebApplicationContext.SCOPE_REQUEST)
 public class ShoppingCartController {
 	
 	private final ShoppingCart shoppingCart;
